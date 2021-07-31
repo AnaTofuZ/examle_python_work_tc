c3b47f6a320a6fa07ce2adbb52ba0ba277583a97
AnaTofuZ <anatofuz@hatena.ne.jp>
2021-07-31 11:38:36 +0900

    ...

--- 3/2/main.py
+++ 3/2/main.py
@@ -2,15 +2,13 @@
 
 
 def main():
-    is_circle = True
     width = 1
-    for i in range(0, 8):
-        is_first_number_circle = is_circle
+    for i in range(1, 8 + 1):
+        is_circle = first_position_is_circle(i)  # 奇数だったらo、偶数ならx
         for _ in range(0, width):
             print(circle_or_cross(is_circle), end='\t')
             is_circle = not is_circle  # 右隣はoが来てたらxになる
         width += 1
-        is_circle = not is_first_number_circle  # 各行の最初のoxは、直前の行の先頭と逆
         print('')
 
 
@@ -18,6 +16,12 @@ def circle_or_cross(is_circle):
     if is_circle:
         return 'o'
     return 'x'
+
+
+# 奇数だったらo、偶数ならx
+# oのときはis_circleがTrueなので、奇数だったらTrueを返す
+def first_position_is_circle(width):
+    return width % 2 != 0
 
 
 if __name__ == "__main__":
